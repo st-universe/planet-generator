b4a8ba3b45853309155189d421c6ae61520e84b0
Huxinator <nicotintel@gmail.com>
2023-10-18 20:15:45 +0200

    fix

--- src/PlanetGenerator/coldata/317.php
+++ src/PlanetGenerator/coldata/317.php
@@ -214,7 +214,7 @@ $ophase[$ophases][PlanetGenerator::COLGEN_MODE] = "upper orbit";
 $ophase[$ophases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lower Orbit";
 $ophase[$ophases][PlanetGenerator::COLGEN_NUM] = 10;
 $ophase[$ophases][PlanetGenerator::COLGEN_FROM] = array("0" => "900");
-$ophase[$ophases][PlanetGenerator::COLGEN_TO] = array("0" => "917");
+$ophase[$ophases][PlanetGenerator::COLGEN_TO] = array("0" => "963");
 $ophase[$ophases][PlanetGenerator::COLGEN_ADJACENT] = 0;
 $ophase[$ophases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
 $ophase[$ophases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
